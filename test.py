8fad831dc62a492edf443be099381246b26c26b0
Tyler Stevahn <tylerjay1996@hotmail.com>
2023-11-02 09:09:13 -0500

    cleanup unused fies

--- test.py
+++ test.py
@@ -1,58 +1,45 @@
-import sys
+import random
 import chess
 import chess.engine
-import random
+import time
 
-def uci_loop():
-    while True:
-        command = input()
-        if command == "uci":
-            print("uciok")
-        if command == "quit":
-            break
-        elif command == "isready":
-            print("readyok")
-        elif command.startswith("position"):
-            # Handle setting the board position
-            handle_position(command)
-        elif command.startswith("go"):
-            # Handle starting the engine's search
-            handle_go(command)
-        '''
+board = chess.Board()
+# print(board)
+run = True
+
+while run:
+    message = input()
+    # this is for the uciok message
+    if message == "uci":
+        print("id Random_moves")
+        print("uciok")
+
+    # this is for the isready message
+    if message == "isready":
+        print("readyok")
+
+    # this is for the ucinewgame message
+    if message == "ucinewgame":
+        #board = chess.Board()
+        moves = [] 
+        print("readyok")
+    
+     # this is for the position message
+    if message.startswith("position startpos"):
+        parts = message.split()
+        if "moves" in parts:
+            moves = parts[parts.index("moves") + 1:]
+            board = chess.Board() 
+            for move in moves:
+                board.push_uci(move)
+        
+    # this is for the go message
+    if message.startswith("go"):
+        legal_moves = list(board.legal_moves)
+        # print(legal_moves)
+        if legal_moves:
+            best_move = random.choice(legal_moves)
+            time.sleep(0.25)
+            print("bestmove", best_move)
         else:
-            print("Unknown command")
-            '''
-
-def handle_position(command):
-    parts = command.split()
-    if "startpos" in parts:
-        board = chess.Board()
-    else:
-        # Handle setting a custom position (e.g., "position fen <FEN>")
-        fen_index = parts.index("fen") + 1
-        fen = " ".join(parts[fen_index:])
-        board = chess.Board(fen)
-
-    moves_index = parts.index("moves") if "moves" in parts else -1
-    if moves_index != -1:
-        # Handle applying a list of moves to the current position
-        moves = parts[moves_index + 1:]
-        for move in moves:
-            board.push_uci(move)
-
-def handle_go(command):
-    # Basic implementation, just return a random move
-    board = chess.Board()
-    if "ponder" in command:
-        print("bestmove {} ponder {}".format(get_random_move(board), get_random_move(board)))
-    else:
-        print("bestmove {}".format(get_random_move(board)))
-
-def get_random_move(board):
-    legal_moves = list(board.legal_moves)
-    return random.choice(legal_moves).uci()
-
-if __name__ == "__main__":
-    #print("id name Chatgpt_Engine_1")
-    #print("id author Chatgpt")
-    uci_loop()
+            print("bestmove (none)")  # No legal moves left
