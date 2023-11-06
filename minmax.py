148845050b12c37b2e99f33756a4402b3f6e29d8
Tyler Stevahn <tylerjay1996@hotmail.com>
2023-11-06 08:44:56 -0600

    testing updated minmax and added log file for stockfish 16 for UCI referencing.

--- minmax.py
+++ minmax.py
@@ -1,11 +1,18 @@
 import chess
 
+#Material
+# King Safety
+# Activity of Pieces
+# Pawn Structure
+# Space Advantage
+
 PV = {
     'pawn': 100,
     'knight': 320,
     'bishop': 330,
     'rook': 500,
-    'queen': 950
+    'queen': 950,
+    'king': 99999
 }
 
 DRAW_VALUE = 0
@@ -29,9 +36,12 @@ def evaluation(board, turn):
     wq = len(board.pieces(chess.QUEEN, chess.WHITE))
     bq = len(board.pieces(chess.QUEEN, chess.BLACK))
 
+    wk = len(board.pieces(chess.KING, chess.WHITE))
+    bk = len(board.pieces(chess.KING, chess.BLACK))
+
     value = (
-        PV['pawn'] * (wp - bp) + PV['knight'] * (wn - bn) + PV['bishop'] * (wb - bb) + PV['rook'] * (wr - br) + PV['queen'] * (wq - bq) #-
-        #PV['pawn'] * (bp - wp) + PV['knight'] * (bn - wn) + PV['bishop'] * (bb - wb) + PV['rook'] * (br - wr) + PV['queen'] * (bq - wq)
+        PV['pawn'] * (wp - bp) + PV['knight'] * (wn - bn) + PV['bishop'] * (wb - bb) + PV['rook'] * (wr - br) + PV['queen'] * (wq - bq)  + PV['king'] * (wk - bk)#-
+        #PV['pawn'] * (bp - wp) + PV['knight'] * (bn - wn) + PV['bishop'] * (bb - wb) + PV['rook'] * (br - wr) + PV['queen'] * (bq - wq) + PV['king'] * (bk - wk)
         )
     '''
     if value != 0:
@@ -39,7 +49,7 @@ def evaluation(board, turn):
     '''
     return value
 
-def minmax(board, depth, maximizing_player):
+def minmax(board, depth, alpha, beta, maximizing_player):
     # print("Starting Minmax")
     if depth == 0 or board.is_game_over():
         return evaluation(board, maximizing_player)
@@ -50,7 +60,13 @@ def minmax(board, depth, maximizing_player):
         for move in list(board.legal_moves):
             board_copy = board.copy()
             board_copy.push(move)
-            val = minmax(board_copy, depth - 1, False)  # Switch to minimizing player
+            val = minmax(board_copy, depth - 1, alpha, beta, False)  # Switch to minimizing player
+            alpha = max(alpha, best)
+            alphacurr = alpha
+            if alphacurr < alpha:
+                print("alpha", alphacurr, "for move ", move)
+            if beta <= alpha:
+                break
             best = max(best, val)
 
     else:  # Minimizing player
@@ -59,7 +75,13 @@ def minmax(board, depth, maximizing_player):
         for move in list(board.legal_moves):
             board_copy = board.copy()
             board_copy.push(move)
-            val = minmax(board_copy, depth - 1, True)  # Switch to maximizing player
+            val = minmax(board_copy, depth - 1, alpha, beta, True)  # Switch to maximizing player
+            beta = min(beta, best)
+            betacurr = beta
+            if betacurr > beta:
+                print("beta", betacurr, "for move ", move)
+            if beta <= alpha:
+                break
             best = min(best, val)
 
     return best
--- test.py
+++ test.py
@@ -7,25 +7,29 @@ from Pos_Eval import engine_evaluation
 import os
 
 
+depth = 3
+
 # print("starting")
 
 board = chess.Board()
 # print(board)
 run = True
 
-def select_best_move(board, depth, maximizing_player):
+def select_best_move(board, depth, alpha, beta, maximizing_player):
+    #print("list moves")
     legal_moves = list(board.legal_moves)
     best_move = None
     if maximizing_player:
         best_score = -float(10000) 
     else:
         best_score = float(10000)
+    #print("Set Score")
 
     for move in legal_moves:
         board_copy = board.copy()
         board_copy.push(move)
-        move_score = minmax(board_copy, depth - 1, not maximizing_player)
-        print("testing", move, move_score, best_score)  
+        move_score = minmax(board_copy, depth - 1, alpha, beta, not maximizing_player)
+        print("info score cp", move_score) #Stockfish prints  info depth 2 seldepth 2 multipv 1 score cp 910 nodes 116 nps 116000 hashfull 0 tbhits 0 time 1 pv d7c6
         # White is maximizing_player
         if maximizing_player:
             if move_score > best_score:
@@ -44,30 +48,31 @@ def select_best_move(board, depth, maximizing_player):
 while not board.is_game_over():
     message = input()
     #with open('C:/Users/tstevahn/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-    with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-        file.write(message + os.linesep)
-        file.close()
+    # with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
+    #     file.write(message + os.linesep)
+    #     file.close()
     if message == "quit":
         break
     # this is for the uciok message
     if message == "uci":
         print("id Random_moves")
         #with open('C:/Users/tstevahn/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-        with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-            file.write("id Random_moves" + os.linesep)
-            file.close()    
+        #with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
+        #    file.write("id Random_moves" + os.linesep)
+        #    file.close()
+        # print("setoptions name depth 3")
         print("uciok")
         #with open('C:/Users/tstevahn/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-        with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-            file.write("uciok" + os.linesep)
-            file.close()    
+        #with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
+        #    file.write("uciok" + os.linesep)
+        #    file.close()    
     # this is for the isready message
     if message == "isready":
         print("readyok")
         #with open('C:/Users/tstevahn/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-        with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-            file.write("readyok" + os.linesep)
-            file.close()    
+        #with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
+        #    file.write("readyok" + os.linesep)
+        #    file.close()    
 
     # this is for the ucinewgame message
     if message == "ucinewgame":
@@ -75,9 +80,9 @@ while not board.is_game_over():
         moves = [] 
         print("readyok")
         # with open('C:/Users/tstevahn/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-        with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
-            file.write("readyok" + os.linesep)
-            file.close()    
+        #with open('C:/Users/Snick/Documents/To_Laptop/Chess-Engines/log.txt', 'a') as file:
+        #    file.write("readyok" + os.linesep)
+        #    file.close()    
     
      # this is for the position message
     if message.startswith("position startpos"):
@@ -95,23 +100,23 @@ while not board.is_game_over():
                 turn = "black"
             else:
                 turn = "white"
+        # print(turn)
         
     # this is for the go message
     if message.startswith("go"):
-        legal_moves = list(board.legal_moves)
-        # print(legal_moves)
-        high_score = -1000000
-        move_score = -2000
+        print("go")
         # with open("C:/Users/tstevahn/To_Laptop/Chess-Engines/fen_test.txt", 'a') as file: # Desktop 
-        with open("C:/Users/Snick/Documents/To_Laptop/Chess-Engines/fen_test.txt", 'a') as file: # Laptop
-            file.write(board.fen() + os.linesep)
-        if legal_moves:
-            if turn == "white":
-                best_move = select_best_move(board, 2, True)
-            else:
-                best_move = select_best_move(board, 2, False)
-
-            print("bestmove", best_move)
-            time.sleep(0.05)
+        # with open("C:/Users/Snick/Documents/To_Laptop/Chess-Engines/fen_test.txt", 'a') as file: # Laptop
+        #     file.write(board.fen() + os.linesep)
+        #     file.close
+        
+        if turn == "white":
+            best_move = select_best_move(board, depth, -10000, 10000, True)
         else:
+            best_move = select_best_move(board, depth, -10000, 10000, False)
+
+        print("bestmove", best_move)
+        # time.sleep(0.05)
+        legal_moves = list(board.legal_moves)
+        if not board.legal_moves:
             print("bestmove (none)")  # No legal moves left
